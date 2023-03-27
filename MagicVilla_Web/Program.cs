e98d8c09950a9bc352ec5b19db82b08cef322815
Nirajan Karki <nirajankarki1985@gmail.com>
2023-03-27 20:14:44 +0200

    Creating baseservice

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,6 +1,9 @@
+using MagicVilla_Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,33 @@
+﻿using MagicVilla_Web.Models;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+        public BaseService(IHttpClientFactory httpClient) 
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+
+        }
+
+        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client= httpClient.CreateClient("MagicAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                if(apiRequest.Data!= null)
+                {
+                    message.Content=new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        encoding.UTF8, "application/json");
+                }
+            }
+        }
+    }
+}
