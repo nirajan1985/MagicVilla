9223ad578b11ff807fdce9e046aa867008b4850b
Nirajan Karki <nirajankarki1985@gmail.com>
2023-04-02 09:22:32 +0200

    fixing bug on registering new user

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -17,12 +17,14 @@ namespace MagicVilla_VillaAPI.Repository
         private string secretKey;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IMapper _mapper;
-        public UserRepository(ApplicationDbContext db,IConfiguration configuration,UserManager<ApplicationUser> userManager,IMapper mapper)
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserRepository(ApplicationDbContext db,IConfiguration configuration,UserManager<ApplicationUser> userManager,IMapper mapper, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
             secretKey = configuration.GetValue<string>("ApiSettings:Secret");
             _userManager = userManager;
             _mapper = mapper;
+            _roleManager = roleManager;
         }
 
         public bool IsUniqueUser(string username)
@@ -90,6 +92,11 @@ namespace MagicVilla_VillaAPI.Repository
                 var result=await _userManager.CreateAsync(user,registrationRequestDTO.Password);
                 if (result.Succeeded)
                 {
+                    if(!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("admin"));
+                        await _roleManager.CreateAsync(new IdentityRole("customer"));
+                    }
                     await _userManager.AddToRoleAsync(user, "admin");
                     var userToReturn=_db.ApplicationUsers.FirstOrDefault(u=>u.UserName== registrationRequestDTO.UserName);
                     return _mapper.Map<UserDTO>(userToReturn);
